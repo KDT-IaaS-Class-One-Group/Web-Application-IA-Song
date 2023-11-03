d2d3b75c0793fc85a5b93df92e8fb9848a894fb2
songyoungjun <clesaaaaaa@gmail.com>
2023-11-03 14:30:34 +0900

    css 정리

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const notesFilePath = path.join(__dirname, 'notes.json');
 if (!fs.existsSync(notesFilePath)) {
   fs.writeFileSync(notesFilePath, '[]');
 }
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -27,7 +28,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  // 새 노트 추가
+
   const { avatar , content } = req.body;
   if (!content) {
     return res.status(400).json({ error: 'content are required.' });
@@ -49,6 +50,23 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+app.delete('/api/notes/delete', (req, res) => {
+  
+  fs.readFile(notesFilePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'An error occurred while reading notes.' });
+    }
+    const notes = JSON.parse(data);
+
+   
+    fs.writeFile(notesFilePath, JSON.stringify(notes, null, 1), (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'An error occurred while deleting the note.' });
+      }
+      res.sendStatus(204); // 성공적으로 삭제된 경우 204 상태 코드 반환
+    });
+  });
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- public/index.js
+++ public/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const now = new Date();	// 현재 날짜 및 시간
   const hours = now.getHours();	// 시간
   const avatar= "👤"
+  const deleteButton= document.getElementById("deleteButton")
   doneButton.addEventListener('click', () => {
     const content = noteContent.value;
     if (content) {
@@ -23,6 +24,28 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
   });
+
+  const dropdown = document.getElementById('sbm');
+  const dropdownContent = dropdown.querySelector('.dropdown-content');
+  const dropbtn = dropdown.querySelector('.dropbtn');
+  
+  // 드롭다운 버튼 클릭 시 드롭다운 내용을 토글
+  dropbtn.addEventListener('click', function() {
+      dropdownContent.classList.toggle('show');
+  });
+  
+  // 다른 곳을 클릭하면 드롭다운이 닫히도록 함
+  window.addEventListener('click', function(event) {
+      if (!event.target.matches('.dropbtn')) {
+          if (dropdownContent.classList.contains('show')) {
+              dropdownContent.classList.remove('show');
+          }
+      }
+  });
+
+
+
+
   function generateUniqueId() {
     const random = Math.floor(Math.random() * 100); 
     return `note-${random}`;
@@ -33,101 +56,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const noteBox = document.createElement('div');
     noteBox.className = 'note-box';
     noteBox.dataset.id = note.id; // Add data attribute for the note's ID
+    noteBox.style.font="Arial, sans-serif"
     noteBox.innerHTML = `"👤"<p>${note.content}</p>`;
     noteBox.id =  generateUniqueId()
     noteBox.setAttribute('data-note-id', note.id);
-    // const contentParagraph = noteBox.querySelector('p');
-    // contentParagraph.addEventListener('click', () => {
-    //   contentParagraph.classList.toggle('selected');
-    // });
-
     noteContainer.appendChild(noteBox);
   }
-
   
-//   function displayEditModal(note) {
-//     const editNoteTitle = document.getElementById('editNoteTitle');
-//     const editNoteContent = document.getElementById('editNoteContent');
-//     const updateButton = document.getElementById('updateButton');
+  deleteButton.addEventListener('click', () => {
+    deleteNote();
+  });
 
-//     // 모달 내용 초기화
-//     if (editNoteTitle && editNoteContent && updateButton) {
-//       editNoteTitle.value = note.title;
-//       editNoteContent.value = note.content;
+  function deleteNote() {
 
-//       // 모달 보이기
-//       editModal.style.display = 'block';
+fetch(`/api/notes/delete`, {
+  method: 'DELETE',
+})
+  .then((response) => {
+    if (response.status === 204) {
+      const noteContainer = document.getElementById('noteContainer');
+      const noteBox = document.querySelector(".note-box");
 
-//       updateButton.addEventListener('click', () => {
-//         const updatedTitle = editNoteTitle.value;
-//         const updatedContent = editNoteContent.value;
-
-//         if (updatedTitle && updatedContent) {
-//           const updatedNoteData = { title: updatedTitle, content: updatedContent };
-
-//           fetch(`/api/notes/${note.id}`, {
-//             method: 'PUT',
-//             headers: {
-//               'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(updatedNoteData),
-//           })
-//             .then((response) => response.json())
-//             .then((updatedNote) => {
-//               editModal.style.display = 'none';
-
-//               // Update the note on the page
-//               note.title = updatedTitle;
-//               note.content = updatedContent;
-//               const noteBox = document.querySelector('.note-box[data-id="' + note.id + '"]');
-//               noteBox.innerHTML = `<h3>${updatedTitle}</h3><p>${updatedContent}</p>`;
-//             });
-//         }
-//       })
-//       deleteButton.addEventListener('click', () => {
-//       deleteNote(note.id);
-//     });
-
-//     function deleteNote(noteId) {
-//   // 서버에서 노트 삭제 요청을 보내는 코드 작성
-//   fetch(`/api/notes/${noteId}`, {
-//     method: 'DELETE',
-//   })
-//     .then((response) => {
-//       if (response.status === 204) {
-//         // 삭제가 성공하면 페이지에서 노트 박스 제거
-//         const noteBox = document.querySelector('.note-box[data-id="' + noteId + '"]');
-//         if (noteBox) {
-//           noteContainer.removeChild(noteBox);
-//           // 모달 닫기
-//           editModal.style.display = 'none';
-//         }
-//       } else {
-//         console.error('Failed to delete the note.');
-//       }
-//     })
-//     .catch((error) => {
-//       console.error('An error occurred while deleting the note:', error);
-//     });
-// }
-//       if (!closeEditModalButton) {
-//         // 닫기 버튼이 없을 때만 추가
-//         const closeModalButton = document.createElement('button');
-//         closeModalButton.id = 'closeEditModal'; // ID 설정
-//         closeModalButton.addEventListener('click', () => {
-//           editModal.style.display = 'none';
-//         });
-//         closeModalButton.textContent = 'Close';
-//         editModal.appendChild(closeModalButton);
-//       }
-//       closeEditModalButton.addEventListener('click', () => {
-//         editModal.style.display = 'none';
-//       });
-//     }};
-  
-  
-
-  // 초기 노트 목록을 불러오기
+        while(noteContainer.hasChildNodes()){
+        noteContainer.removeChild(noteBox);}
+      }
+    
+  })
+  .catch((error) => {
+    console.error('An error occurred while deleting the note:', error);
+  });
+}
   fetch('/api/notes')
     .then((response) => response.json())
     .then((notes) => {
