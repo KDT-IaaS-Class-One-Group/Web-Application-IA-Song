d1ef7b23b8bd2e00927c6dd3bf0a66b8fca3feb8
songyoungjun <clesaaaaaa@gmail.com>
2023-11-03 18:05:35 +0900

    일단마무리

--- public/index.js
+++ public/index.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const noteContent = document.getElementById('noteContent');
   const doneButton = document.getElementById('doneButton');
   const noteContainer = document.getElementById('noteContainer');
-  const now = new Date();	// 현재 날짜 및 시간
-  const hours = now.getHours();	// 시간
+  const now = new Date();	
+  const hours = now.getHours();	
   const avatar= "👤"
   const deleteButton= document.getElementById("deleteButton")
   doneButton.addEventListener('click', () => {
@@ -29,12 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const dropdownContent = dropdown.querySelector('.dropdown-content');
   const dropbtn = dropdown.querySelector('.dropbtn');
   
-  // 드롭다운 버튼 클릭 시 드롭다운 내용을 토글
+  
   dropbtn.addEventListener('click', function() {
       dropdownContent.classList.toggle('show');
   });
   
-  // 다른 곳을 클릭하면 드롭다운이 닫히도록 함
+
   window.addEventListener('click', function(event) {
       if (!event.target.matches('.dropbtn')) {
           if (dropdownContent.classList.contains('show')) {
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
    function displayNoteBox(note) {
     const noteBox = document.createElement('div');
     noteBox.className = 'note-box';
-    noteBox.dataset.id = note.id; // Add data attribute for the note's ID
+    noteBox.dataset.id = note.id; 
     noteBox.style.font="Arial, sans-serif"
     noteBox.innerHTML = `"👤"<p>${note.content}</p>`;
     noteBox.id =  generateUniqueId()
